3c9a7f8abd9f36a000f19c0de30af80b75c97e52
tanyuhao1 <tanyuhao1@ailyun.com>
2019-04-22 08:50:53 +0800

    TestGit3添加一句话且是新的分支TestGit3Branch

--- src/main/java/com/shopping/test/Apple.java
+++ src/main/java/com/shopping/test/Apple.java
@@ -4,5 +4,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("你好456");
 		System.out.println("TestGit1添加一句话 ,TestGit2添加第二句话 ");
+		System.out.println("TestGit3添加一句话");
 	}
 }

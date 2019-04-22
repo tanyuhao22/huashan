5285891ea6714efe38923e1341e19d916e6adac1
tanyuhao1 <tanyuhao1@ailyun.com>
2019-04-22 08:43:15 +0800

    TestGit2添加第二句话

--- src/main/java/com/shopping/test/Apple.java
+++ src/main/java/com/shopping/test/Apple.java
@@ -2,6 +2,7 @@ package com.shopping.test;
 
 public class Apple {
 	public static void main(String[] args) {
-		System.out.println("你好");
+		System.out.println("你好456");
+		System.out.println("TestGit2添加第二句话");
 	}
 }

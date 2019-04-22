7f976fde4b270183a1241e0a4cd97631b93e9211
tanyuhao1 <tanyuhao1@ailyun.com>
2019-04-22 08:41:15 +0800

    TestGit添加一句话

--- src/main/java/com/shopping/test/Apple.java
+++ src/main/java/com/shopping/test/Apple.java
@@ -2,6 +2,8 @@ package com.shopping.test;
 
 public class Apple {
 	public static void main(String[] args) {
-		System.out.println("你好");
+		System.out.println("你好123123213"); 
+		System.out.println("添加一句话");
+		
 	}
 }

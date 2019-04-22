8c69f3ffff3c6cd50dfeca60b309aaa2714d1f03
tanyuhao1 <tanyuhao1@ailyun.com>
2019-04-22 09:06:48 +0800

    TestGit在本地合并之后再提交

--- src/main/java/com/shopping/test/Apple.java
+++ src/main/java/com/shopping/test/Apple.java
@@ -5,5 +5,6 @@ public class Apple {
 		System.out.println("你好456");
 		System.out.println("TestGit1添加一句话 ,TestGit2添加第二句话 ");
 		System.out.println("TestGit3添加一句话");
+		System.out.println("合并之后再添加新的语句");
 	}
 }

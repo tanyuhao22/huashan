28330f0bdeca7ec04eb7fe5357efd699474b24cb
tanyuhao1 <tanyuhao1@ailyun.com>
2019-04-23 14:02:38 +0800

    Commit Ok HUASAN

--- src/main/java/com/shopping/test/Apple.java
+++ src/main/java/com/shopping/test/Apple.java
@@ -6,5 +6,6 @@ public class Apple {
 		System.out.println("TestGit1添加一句话 ,TestGit2添加第二句话 ");
 		System.out.println("TestGit3添加一句话");
 		System.out.println("合并之后再添加新的语句");
+		System.out.println("我已经搞定了。");
 	}
 }
---
+++ src/main/java/com/shopping/test/TestMain.java
@@ -0,0 +1,11 @@
+package com.shopping.test;
+
+import org.junit.Test;
+
+public class TestMain {
+
+		@Test
+		public void testName() throws Exception {
+			System.out.println("输出了一句话");
+		}
+}
